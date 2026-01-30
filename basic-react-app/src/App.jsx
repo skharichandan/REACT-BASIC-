f354ada58a100eea81c82f74a826bc0479c27c09
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-30 19:08:39 +0530

    Intoduction to Components

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,8 +1,21 @@
 import './App.css'
 
-function App() {
+function Title() {
+  return <h1>I am the Title!</h1>
+}
+function Description() {
+  return <h3>I am the Description</h3>
+}
 
-  return <h1>Hello World</h1>
+function App() {
+  return (
+    <div>
+      <h1>This is my app component</h1>
+      <p>inside app component we have : </p>
+      <Title/>
+      <Description/>
+    </div>
+  );
 }
 
 export default App
