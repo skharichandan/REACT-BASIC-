42b57936916d2ecbaba8e5e4dab1e703a85b5f1b
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-30 20:10:50 +0530

    Without react fragment

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
 import Title from "./Title.jsx";
-import Description2 from "./Description.jsx";
 
 
 
 function App() {
   return (
-    <div>
-      <h1>This is my app component</h1>
-      <p>inside app component we have : </p>
+    <div className='mainbox'>
       <Title/>
-      <Description2/>
+      <Title/>
+      <Title/>
     </div>
   );
 }
