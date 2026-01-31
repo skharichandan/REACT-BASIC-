46fcbc4952e83ac15b864f96559709646b5fbf5f
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-31 12:42:13 +0530

    Component Styling

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,9 +1,9 @@
 import "./Product.css"
 
 function Product({title,price}) {
-    
+   let styles = {backgroundColor : "blue"}; 
     return (
-        <div className="Product">
+        <div className="Product" style = {styles}>
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
             {price > 30000 && <p>Discount of 5%</p>}
