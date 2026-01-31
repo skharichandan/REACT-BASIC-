e1455dab40e6020ef451b2395e69d5d2975de190
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-31 10:30:09 +0530

    Passing Arrays to props - 2

--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -2,12 +2,12 @@ import Product from "./Product"
 
 function ProductTab() {
     let options = ["slimmest in this segment","jumbo performance","smooth writing"];
-    let options2 = {a : "SLIMMEST" , b : "PERFORMANCE" , c : "SMOOTH"}
+
     return (
         <>
-        <Product title = "phone" price = {35000} features = {options[0]} features2 = {options2.a}/>
-        <Product title = "laptop" price = {60000} features = {options[1]} features2 = {options2.b}/>
-        <Product title = "pen" price = {45} features = {options[2]} features2 = {options2.c}/>
+        <Product title = "phone" price = {35000} features = {["slimmest in this segment","jumbo performance","smooth writing"]} features2 = {{a:"SLIMMEST",B:"PERFORMANCE",c:"SMOOTH"}}/>
+        {/* <Product title = "laptop" price = {60000} features = {options[1]} features2 = {options2}/> */}
+        {/* <Product title = "pen" price = {45} features = {options[2]} features2 = {options2}/> */}
         </>
     )
 }
