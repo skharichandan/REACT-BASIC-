e3273ed040b57578261e63ef859081f6b5517601
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-31 12:34:12 +0530

    Conditionals-4

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -6,7 +6,7 @@ function Product({title,price}) {
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
-            {price > 30000 ? <p>"Discount of 5%"</p> : <a href="">Get Discount</a>}
+            {price > 30000 && <p>Discount of 5%</p>}
         </div>
     );  
 }
