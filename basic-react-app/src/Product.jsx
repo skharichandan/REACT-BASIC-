507d49250485dfaff2cac26d36b0819b0ec045de
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-31 09:57:21 +0530

    React props-2

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,11 +1,10 @@
 import "./Product.css"
 
-function Product(props) {
-    // console.log(props)
+function Product({title,price}) {
     return (
         <div className="Product">
-            <h3>{props.title}</h3>
-            <h5>{props.price}</h5>
+            <h3>{title}</h3>
+            <h5>Price : {price}</h5>
         </div>
     );
 }
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product"
 function ProductTab() {
     return (
         <>
-        <Product title = "phone" price = "35,000"/>
-        <Product title = "laptop" price = "60,000"/>
-        <Product title = "pen" price = "45"/>
+        <Product title = "phone" price = {35000}/>
+        <Product title = "laptop" price = {60000}/>
+        <Product title = "pen" price = {45}/>
         </>
     )
 }
