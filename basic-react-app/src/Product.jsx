b7186e03730f7637024979b89901b9b39a9a491b
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-01-31 12:31:18 +0530

    Conditionals-3

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,13 +1,12 @@
 import "./Product.css"
 
 function Product({title,price}) {
-    let isDiscount = price > 40000 ? "Discount of 5%" : "";
     
     return (
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
-            <p>{isDiscount}</p>
+            {price > 30000 ? <p>"Discount of 5%"</p> : <a href="">Get Discount</a>}
         </div>
     );  
 }
